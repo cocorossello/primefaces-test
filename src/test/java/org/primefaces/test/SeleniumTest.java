4046b895aeddbaedbc50b435863f1e3b0a01ed34
vrossello <vrossello@travelcompositor.com>
2023-07-03 13:27:40 +0200

    keyfilter regression example

---
+++ src/test/java/org/primefaces/test/SeleniumTest.java
@@ -0,0 +1,17 @@
+package org.primefaces.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SeleniumTest {
+
+    @Test
+    public void ads() {
+        open("http://localhost:8080/primefaces-test/");
+        $(".some-input").getWrappedElement().sendKeys("ج");
+        $(".some-input").shouldBe(Condition.empty);
+    }
+}
