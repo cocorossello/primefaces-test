2c82b79620f92be85024b9183ca1c5766d4e8dfb
vrossello <vrossello@travelcompositor.com>
2023-07-03 12:39:43 +0200

    rowexpansion regression example

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,13 +1,16 @@
 package org.primefaces.test;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.math.BigDecimal;
+import lombok.Data;
+
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
-import lombok.Data;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Named
@@ -24,4 +27,32 @@ public class TestView implements Serializable {
         string = "Welcome to PrimeFaces!!!";
     }
 
+    public List<SomeBean> getSomeBeans() {
+        return List.of(new SomeBean("some string!"));
+    }
+
+    public void confirm() {
+        addMessage("Confirmed", "You have accepted");
+    }
+
+
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public class SomeBean {
+        private String someString;
+
+        public SomeBean() {
+        }
+
+        public SomeBean(String someString) {
+            this.someString = someString;
+        }
+
+        public String getSomeString() {
+            return someString;
+        }
+    }
 }
